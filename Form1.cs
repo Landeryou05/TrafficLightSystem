c9d956290e21dea941525f4059302b7598f04b0e
Landeryou05 <landeryou05@gmail.com>
2024-05-13 11:21:32 +0100

    Optimised forms and renamed files

--- Form1.cs
+++ Form1.cs
@@ -9,8 +9,8 @@ namespace SignalAggTask
     public partial class Form1 : Form
     {
         // Instancing the singal box and declaring variables.
-        private SignalBox m_signalBox1;
-        private SignalBox2 m_signalBox2;
+        private TrafficLight1 m_signalBox1;
+        private TrafficLight2 m_signalBox2;
         private int m_signalState1;
         private int m_signalState2;
         private int m_buttonPressed = 0;
@@ -38,10 +38,10 @@ namespace SignalAggTask
             InitializeComponent();
 
             // This class changes the light when Form1 is instanced (From the start).
-            m_signalBox1 = new SignalBox(20, 0);
+            m_signalBox1 = new TrafficLight1(20, 0);
             m_signalBox1.ChangeLights(0);
 
-            m_signalBox2 = new SignalBox2(20, 0);
+            m_signalBox2 = new TrafficLight2(20, 0);
             m_signalBox2.ChangeLights(0);
         }
 
--- TrafficLight1.cs
+++ TrafficLight1.cs
@@ -3,45 +3,53 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SignalAggTask
 {
-    internal class SignalBox2 : Form
+    internal class TrafficLight1
     {
+        // Instance a lamp and store them as variables.
         private Lamp m_redLamp;
         private Lamp m_amberLamp;
         private Lamp m_greenLamp;
 
-        public SignalBox2(int x, int y)
+        public TrafficLight1(int x, int y)
         {
+            // Defining the colour of the lap + defining the size of the lamp.
             m_redLamp = new Lamp(Color.Red, x, y + 25);
             m_amberLamp = new Lamp(Color.Orange, x, y + 85);
             m_greenLamp = new Lamp(Color.Green, x, y + 145);
         }
-
+        /// <summary>
+        ///     These are the different combination of lights
+        ///     They return a value that corresponds to the use case.
+        /// </summary>
+        /// <param name="flag"></param>
+        /// <returns></returns>
         public int ChangeLights(int flag)
-        {
+        {   
             switch (flag)
             {
                 case 0:
-                    m_redLamp.OnStatus = false;
+                    m_redLamp.OnStatus = true;
                     m_amberLamp.OnStatus = false;
-                    m_greenLamp.OnStatus = true;
+                    m_greenLamp.OnStatus = false;
                     return 1;
                 case 1:
-                    m_redLamp.OnStatus = false;
+                    m_redLamp.OnStatus = true;
                     m_amberLamp.OnStatus = true;
                     m_greenLamp.OnStatus = false;
                     return 2;
                 case 2:
-                    m_redLamp.OnStatus = true;
+                    m_redLamp.OnStatus = false;
                     m_amberLamp.OnStatus = false;
-                    m_greenLamp.OnStatus = false;
+                    m_greenLamp.OnStatus = true;
                     return 3;
                 case 3:
-                    m_redLamp.OnStatus = true;
+                    m_redLamp.OnStatus = false;
                     m_amberLamp.OnStatus = true;
                     m_greenLamp.OnStatus = false;
                     return 0;
@@ -50,6 +58,10 @@ namespace SignalAggTask
             }
         }
 
+        /// <summary>
+        ///     This displays the graphics for the lights to the main display.
+        /// </summary>
+        /// <param name="g"></param>
         public void Display(Graphics g)
         {
             m_redLamp.Display(g);
